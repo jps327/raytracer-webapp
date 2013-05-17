9d3d0989c1e573c13525333a1e512275a6e25cdd
Juan Pablo Sarmiento <jps327@cornell.edu>
2013-05-17 13:50:34 -0400

    updated connection

--- models/db_connect.js
+++ models/db_connect.js
@@ -1,6 +1,7 @@
 exports = mongoose = require('mongoose');
 // MongoDB connection
-var mongo_uri = 'mongodb://localhost/raytracer';
+// var mongo_uri = 'mongodb://localhost/raytracer';
+var mongo_uri = 'mongodb://nodejitsu:eadc70cd72250db4d0bbab535dafd6b7@alex.mongohq.com:10018/nodejitsudb7509916564';
 mongoose.connect(mongo_uri);
 
 exports = Schema = mongoose.Schema;
